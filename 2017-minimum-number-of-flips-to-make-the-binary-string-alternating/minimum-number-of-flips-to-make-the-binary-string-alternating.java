1d57cc78b4919a72fc8c8d2a676901531e7856cb
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-03-07 17:58:02 +0530

    Time: 19 ms (95.76%) | Memory: 48.2 MB (25.85%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,22 +1,29 @@
 class Solution {
     public int minFlips(String s) {
-        int n = s.length();
-        String s2 = s + s;
-        int diff1 = 0, diff2 = 0,res = Integer.MAX_VALUE,left = 0;
-        for (int right = 0; right < 2 * n; right++) {
-            char expected1 = (right % 2 == 0) ? '0' : '1';
-            char expected2 = (right % 2 == 0) ? '1' : '0';
-            if (s2.charAt(right) != expected1) diff1++;
-            if (s2.charAt(right) != expected2) diff2++;
-            if (right - left + 1 > n) {
-                char leftExpected1 = (left % 2 == 0) ? '0' : '1';
-                char leftExpected2 = (left % 2 == 0) ? '1' : '0';
-                if (s2.charAt(left) != leftExpected1) diff1--;
-                if (s2.charAt(left) != leftExpected2) diff2--;
-                left++;
-            }
-            if (right - left + 1 == n) res = Math.min(res, Math.min(diff1, diff2));
+    int n=s.length(); 
+    s=s+s;
+    char t[]=s.toCharArray();
+    char  a[]=new char[n+n];
+    char b[]=new char[n+n];
+    for(int i=0;i<n+n;i++){
+        if(i%2==0){
+            a[i]='1';
+            b[i]='0';
+        }else{
+            a[i]='0';
+            b[i]='1';
         }
-        return res;
+    }
+    int f=0,sec=0,ans=Integer.MAX_VALUE;
+    for(int i=0;i<n+n;i++){
+        if(a[i]!=t[i]) f++;
+        if(b[i]!=t[i]) sec++;
+        if(i>=n){
+            if(a[i-n]!=t[i-n]) f--;
+            if(b[i-n]!=t[i-n]) sec--;
+        }
+        if(i>=n-1) ans=Math.min(ans,Math.min(f,sec));
+    }
+    return ans;
     }
 }
