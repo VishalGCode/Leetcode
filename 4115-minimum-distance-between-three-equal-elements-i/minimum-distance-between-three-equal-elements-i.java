2a8a2e93024476c50bc5b8513a46a3d95c3a2dec
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-04-10 19:35:59 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (80.71%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -1,15 +1,15 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
-        int ans = Integer.MAX_VALUE;
-        for (List<Integer> list : map.values()) {
-            if (list.size() < 3) continue;
-            for (int i = 0; i <= list.size() - 3; i++) {
-                int dist = 2 * (list.get(i + 2) - list.get(i));
-                ans = Math.min(ans, dist);
-            }
+        int len=nums.length;
+        int[] last2=new int[len];
+        int res=200;
+        for(int i=0;i<len;i++){
+            int val=nums[i]-1;
+            int pos=i+1,pack=last2[val];
+            int old=pack&255, cur=pack>>8;
+            last2[val]=cur|(pos<<8);
+            if(old>0) res=Math.min(res,(pos-old)<<1);
         }
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        return res==200?-1:res;
     }
 }
