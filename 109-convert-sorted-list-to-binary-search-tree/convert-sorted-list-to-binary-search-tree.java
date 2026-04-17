bfbba90a431ffb64825710365339cbd5bc5c30fd
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-04-17 20:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (27.62%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -1,44 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public TreeNode sortedListToBST(ListNode head) {
-    if(head==null) return null;
-    return convert(head, null);  
+    private ListNode current;
+    public TreeNode sortedListToBST(ListNode head){
+        int size = getSize(head);
+        current = head;
+        return buildTree(0, size - 1);
     }
-    private TreeNode convert(ListNode start, ListNode end){
-        if(start==end) return null;
-        ListNode slow=start;
-        ListNode fast=start;
-        while(fast!=end && fast.next!=end){
-            fast=fast.next.next;
-            slow=slow.next;
+
+    private int getSize(ListNode head){
+        int count = 0;
+        while (head != null){
+            count++;
+            head = head.next;
         }
-        TreeNode head=new TreeNode(slow.val);
-        head.left=convert(start, slow);
-        head.right=convert(slow.next,end);
-        return head;
+        return count;
+    }
+
+    private TreeNode buildTree(int left, int right){
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode leftNode = buildTree(left, mid - 1);
+        TreeNode root = new TreeNode(current.val);
+        root.left = leftNode;
+        current = current.next;
+        root.right = buildTree(mid + 1, right);
+        return root;
     }
 }
