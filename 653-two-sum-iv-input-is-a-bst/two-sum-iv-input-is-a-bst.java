688485757a95cf0800ab7edf1bcfad052b5ada41
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-04-18 13:14:47 +0530

    Time: 2 ms (98.42%) | Memory: 47.4 MB (24.92%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,17 +1,47 @@
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        Set<Integer> set = new HashSet<>();
-        return inorder(root, set, k);
+        BSTIterator left = new BSTIterator(root, false); 
+        BSTIterator right = new BSTIterator(root, true); 
+
+        int i = left.next();
+        int j = right.next();
+
+        while (i < j) {
+            int sum = i + j;
+
+            if (sum == k) return true;
+            else if (sum < k) i = left.next();
+            else j = right.next();
+        }
+
+        return false;
+    }
+}
+
+class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<>();
+    private boolean reverse;
+
+    public BSTIterator(TreeNode root, boolean reverse) {
+        this.reverse = reverse;
+        pushAll(root);
     }
 
-    private boolean inorder(TreeNode root, Set<Integer> set, int k) {
-        if (root == null)
-            return false;
-        if (inorder(root.left, set, k))
-            return true;
-        if (set.contains(k - root.val))
-            return true;
-        set.add(root.val);
-        return inorder(root.right, set, k);
+    private void pushAll(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = reverse ? node.right : node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+
+        if (!reverse) {
+            pushAll(node.right);
+        } else {
+            pushAll(node.left);
+        }
+        return node.val;
     }
 }
