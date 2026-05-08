0e3ce98f28fa6d97e0f5270c97501ae6714a8cc7
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-05-08 09:35:03 +0530

    Time: 5 ms (87.10%) | Memory: 199 MB (43.55%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.java
+++ 3981-jump-game-ix/jump-game-ix.java
@@ -1,20 +1,39 @@
 class Solution {
     public int[] maxValue(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        int[] preMax=new int[n];
-        preMax[0] = nums[0];
+
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        int[] res = new int[n];
+
+        // prefix max
+        pre[0] = nums[0];
         for (int i = 1; i < n; i++) {
-            preMax[i] = Math.max(preMax[i - 1], nums[i]);
+            pre[i] = Math.max(pre[i - 1], nums[i]);
         }
-        int sufMin = Integer.MAX_VALUE;
-        for (int i = n - 1; i >= 0; i--) {
-            if (preMax[i] > sufMin)
-                ans[i] = ans[i + 1];
-            else
-                ans[i] = preMax[i];
-            sufMin = Math.min(sufMin, nums[i]);
+
+        // suffix min
+        suf[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = Math.min(suf[i + 1], nums[i]);
         }
-        return ans;    
+
+        res[n - 1] = pre[n - 1];
+
+        // build answer
+        for (int i = n - 2; i >= 0; i--) {
+
+            // merge segment
+            if (pre[i] > suf[i + 1]) {
+                res[i] = res[i + 1];
+            }
+
+            // new segment
+            else {
+                res[i] = pre[i];
+            }
+        }
+
+        return res;
     }
 }
