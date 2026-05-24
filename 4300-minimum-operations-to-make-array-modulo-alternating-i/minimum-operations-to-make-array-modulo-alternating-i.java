92291bb191c6ca94a88e9955df959c9a1984d42e
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-05-24 10:37:25 +0530

    Time: 8 ms (100.00%) | Memory: 45.1 MB (100.00%) - LeetSync

--- 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.java
+++ 4300-minimum-operations-to-make-array-modulo-alternating-i/minimum-operations-to-make-array-modulo-alternating-i.java
@@ -1,38 +1,31 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int n=nums.length;
-        int[] even = new int[k];
-        int[] odd = new int[k];
-        for(int i=0; i<n; i++){
-            int r=nums[i]%k;
-            if(i%2==0){
-                even[r]++;
-            }else{
-                odd[r]++;
+        int[] even = new int[k], odd = new int[k];
+        for (int i = 0; i < nums.length; i++) {
+            int r = nums[i] % k;
+            if ((i & 1) == 0) even[r]++;
+            else odd[r]++;
+        }
+        int[] costEven = new int[k];
+        int[] costOdd = new int[k];
+        for (int target = 0; target < k; target++) {
+            int eCost = 0;
+            int oCost = 0;
+            for (int r = 0; r < k; r++) {
+                int dist =Math.min((target - r + k) % k,(r - target + k) % k);
+                eCost += even[r] * dist;
+                oCost += odd[r] * dist;
+            }
+            costEven[target] = eCost;
+            costOdd[target] = oCost;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (i == j) continue;
+                ans = Math.min(ans,costEven[i] + costOdd[j]);
             }
         }
-        int ops=Integer.MAX_VALUE;
-        for(int i=0; i<k; i++){
-            for(int j=0; j<k; j++){
-                if(i==j) continue;
-                int currOps=0;
-                for(int r=0; r<k; r++){
-                    if(even[r]>0){
-                        int diff_up=(i-r+k)%k;
-                        int diff_down=(r-i+k)%k;
-                        currOps+=even[r]*Math.min(diff_up,diff_down);
-                    }
-                }
-                for(int r=0; r<k; r++){
-                    if(odd[r]>0){
-                        int diff_up=(j-r+k)%k;
-                        int diff_down=(r-j+k)%k;
-                        currOps+=odd[r]*Math.min(diff_up,diff_down);
-                    }
-                }
-                ops=Math.min(ops,currOps);
-            }
-        }
-        return ops;    
+        return ans;
     }
 }
