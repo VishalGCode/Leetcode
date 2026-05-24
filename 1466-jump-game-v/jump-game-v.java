f263bd779080924ffac023aef527eebc78720540
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-05-24 18:14:52 +0530

    Time: 10 ms (87.43%) | Memory: 46.7 MB (19.43%) - LeetSync

--- 1466-jump-game-v/jump-game-v.java
+++ 1466-jump-game-v/jump-game-v.java
@@ -1,24 +1,38 @@
 class Solution {
     int[] dp;
+    int n;
+
     public int maxJumps(int[] arr, int d) {
-        int n = arr.length;
+        n = arr.length;
         dp = new int[n];
         int ans = 1;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             ans = Math.max(ans, dfs(i, arr, d));
         }
         return ans;
     }
-    private int dfs(int i, int[] arr, int d){
-        if(dp[i] !=0) return dp[i];
-        int best =1;
-        for(int nxt = i+1; nxt<=Math.min(arr.length - 1, i+d); nxt++){
-            if(arr[nxt] >= arr[i]) break;
-            best = Math.max(best, 1 + dfs(nxt, arr, d));
-        }
-        for(int nxt = i-1; nxt >= Math.max(0, i-d); nxt--){
-            if(arr[nxt] >= arr[i]) break;
-            best = Math.max(best, 1+dfs(nxt, arr, d));
+
+    // DFS function to calculate maximum jumps from index i
+    private int dfs(int i, int[] arr, int d) {
+        if (dp[i] != 0) return dp[i];
+
+        // Minimum jump count is 1
+        int best = 1;
+        int[] dir = {-1, 1};
+        for (int move : dir) {
+            for (int step = 1; step <= d; step++) {
+                int nxt = i + move * step;
+
+                // Stop if out of bounds
+                if (nxt < 0 || nxt >= n) break;
+
+                // Cannot jump to equal or taller element
+                // Also blocks further movement in this direction
+                if (arr[nxt] >= arr[i]) break;
+
+                // Recursively calculate best path
+                best = Math.max(best,1 + dfs(nxt, arr, d));
+            }
         }
         return dp[i] = best;
     }
