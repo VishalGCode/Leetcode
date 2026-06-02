ea6323348438433e462b0ff8cf9424b53038587b
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-06-02 10:08:48 +0530

    Time: 3 ms (37.80%) | Memory: 43.9 MB (32.93%) - LeetSync

---
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkEqualPartitions(int[] nums, long target) {
+        long  prod1=1;
+        for(int i=0; i<nums.length; i++){
+            prod1 = prod1 * nums[i];
+        }        
+        if(prod1 != target*target) return false;
+        else{
+            return dfs(nums, 0, 1L, 0, target);
+        }
+    }
+    private boolean dfs(int[] nums, int ind, long prod, int count, long target){
+        if(prod == target) return count > 0 && count < nums.length;
+        if(ind == nums.length) return false;
+        if(prod > target) return false;
+        if(prod % prod !=0) return false;
+        if(prod <= target / nums[ind]){
+            if(dfs(nums, ind+1, prod*nums[ind], count+1, target)){
+                return true;
+            }
+        }
+        return dfs(nums, ind+1, prod, count, target);
+    }
+}
