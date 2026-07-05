34ac50a922f4b145922be586788cfaa9a18ab2f7
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-07-05 19:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (85.33%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,10 +1,6 @@
 class Solution {
     int i=0;
     public String decodeString(String s) {
-        //i=0;
-        return decode(s);    
-    }
-    public String decode(String s){
         StringBuilder sb = new StringBuilder();
         int num=0;
         while(i<s.length()){
@@ -14,7 +10,7 @@ class Solution {
                 i++;
             }else if(c == '[') {
                 i++;
-                String inner = decode(s);
+                String inner = decodeString(s);
                 for(int k=0;k<num;k++) sb.append(inner);
                 num=0;
             }else if(c==']'){
@@ -25,6 +21,29 @@ class Solution {
                 i++;
             }
         }
-        return sb.toString();
+        return sb.toString();    
     }
+    // public String decode(String s){
+    //     StringBuilder sb = new StringBuilder();
+    //     int num=0;
+    //     while(i<s.length()){
+    //         char c=s.charAt(i);
+    //         if(Character.isDigit(c)){
+    //             num = num*10 + (c-'0');
+    //             i++;
+    //         }else if(c == '[') {
+    //             i++;
+    //             String inner = decode(s);
+    //             for(int k=0;k<num;k++) sb.append(inner);
+    //             num=0;
+    //         }else if(c==']'){
+    //             i++;
+    //             return sb.toString();
+    //         }else{
+    //             sb.append(c);
+    //             i++;
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
 }
