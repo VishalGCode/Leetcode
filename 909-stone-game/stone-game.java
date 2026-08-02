48a66baf7caa9ed7d97d56df5fb32b54131910be
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-08-02 10:38:50 +0530

    Time: 7 ms (21.79%) | Memory: 47 MB (10.85%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-    return true;    
+    int n=piles.length;
+    int[][] dp = new int[n+1][n+1];
+    return solve(piles,0,n-1,dp)>0;
+    }
+    private int solve(int[] piles, int st, int end, int[][] dp){
+    if(st==end) return piles[st];
+    if(dp[st][end] != 0) return dp[st][end];
+    int left=piles[st]-solve(piles,st+1,end,dp);
+    int right=piles[end]-solve(piles,st,end-1,dp);
+    return dp[st][end]=Math.max(left,right);
     }
 }
