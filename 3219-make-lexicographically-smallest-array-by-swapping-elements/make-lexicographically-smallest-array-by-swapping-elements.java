7ec2a8c138477d28954105d50f8907f2e5dd74bd
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-08-29 09:15:29 +0530

    Time: 84 ms (65.91%) | Memory: 118.5 MB (94.70%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -6,19 +6,19 @@ class Solution {
             sorted[i][0]=nums[i];
             sorted[i][1]=i;
         }
-        java.util.Arrays.sort(sorted,(a,b)->Integer.compare(a[0],b[0]));
+        Arrays.sort(sorted,(a,b)->Integer.compare(a[0],b[0]));
 
         int[] result=new int[n];
         int i=0;
         while(i<n){
             int j=i;
-            java.util.List<Integer> indices=new java.util.ArrayList<>();
+            List<Integer> indices=new ArrayList<>();
             indices.add(sorted[i][1]);
             while(j+1<n&&sorted[j+1][0]-sorted[j][0]<=limit){
                 j++;
                 indices.add(sorted[j][1]);
             }
-            java.util.Collections.sort(indices);
+            Collections.sort(indices);
             for(int k=0;k<indices.size();k++){
                 result[indices.get(k)]=sorted[i+k][0];
             }
