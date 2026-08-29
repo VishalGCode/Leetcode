76640ea82b7bb79ae8a7f1967ab2792d8647ad73
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-08-29 09:28:24 +0530

    Time: 141 ms (63.95%) | Memory: 47 MB (36.05%) - LeetSync

---
+++ 942-super-palindromes/super-palindromes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int superpalindromesInRange(String left, String right) {
+    long l=Long.parseLong(left);
+    long r=Long.parseLong(right);
+    int count=0;
+    int limit=100000;
+    for(int i=1;i<limit;i++){
+        String s=Integer.toString(i);
+        StringBuilder sb=new StringBuilder(s);
+        for(int j=s.length()-2;j>=0;j--) sb.append(s.charAt(j));
+        long k=Long.parseLong(sb.toString());
+        long sq=k*k;
+        if(sq>r) break;
+        if(sq>=l && isPalindrome(sq)) count++;
+    }    
+    for(int i=1;i<limit;i++){
+        String s=Integer.toString(i);
+        StringBuilder sb=new StringBuilder(s);
+        for(int j=s.length()-1;j>=0;j--) sb.append(s.charAt(j));
+        long k=Long.parseLong(sb.toString());
+        long sq=k*k;
+        if(sq>r) break;
+        if(sq>=l && isPalindrome(sq)) count++;
+    }
+    return count;
+    }
+
+    private boolean isPalindrome(long num){
+        String s=Long.toString(num);
+        int left=0, right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
