ac74529f238fe1313ae7247aee2e391848ebf352
VISHAL GUPTA <vishal3555gupta@gmail.com>
2026-09-02 18:24:49 +0530

    Time: 1 ms (31.89%) | Memory: 45.1 MB (57.87%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-    return true;    
+        int odd = -1;
+        for(int x:nums1){
+            if(x%2 != 0){
+                odd=x;
+                break;
+            }
+        }
+        int[] nums2=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i] %2 == 0){
+                if(odd != -1) nums2[i] = nums1[i]-odd;
+                else nums2[i]=nums1[i]-odd;
+            }else nums2[i]=nums1[i];
+        }
+        int target=Math.abs(nums2[0]%2);
+        for(int x:nums2){
+            if(Math.abs(x%2) != target) return false;
+        }
+        return true;
     }
 }
